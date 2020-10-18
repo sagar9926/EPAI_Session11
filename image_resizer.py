5e26dae941b80730c269b118ada1ee761f82203d
sagar9926 <sagarkumar9926@gmail.com>
2020-10-18 21:18:09 +0530

    Update image_resizer.py

--- image_resizer.py
+++ image_resizer.py
@@ -5,67 +5,67 @@ import os
 
 print(f'loading image_resizer.py: __name__ = {__name__}')
 
-def image_resize(image_name,image_path,res_p,res_w,res_h) :
+def image_resize(dir_path,res_p,res_w,res_h) :
 
 	"""
+	dir_path : Path of the folder in which the the images to be resized are present
 	res_p : Resize by user determined percentage (proportional)
 	res_w : resize by user determined width (proportional) 
 	res_h : resize by user determined height (proportional) 
 	
 	"""
+	all_files_in_dir = os.listdir(path = dir_path)
+	all_images_in_dir = [image  for image in all_files_in_dir if ('.jpg' in image.lower() ) or ('.png' in image.lower()) or ('.jpeg' in image.lower())]
 	
-	image_rel_path = os.path.join(image_path,image_name)
-	im = Image.open(image_rel_path)
-
-	if not (res_p is None):
+	for image_name in all_images_in_dir:
 		
-		#Make the new image half the width and half the height of the original image
-		resized_im = im.resize((round(im.size[0]*res_p), round(im.size[1]*res_p)))
+		image_rel_path = os.path.join(dir_path,image_name)
+		im = Image.open(image_rel_path)
 
-		#Save the cropped image
-		resized_im.save(os.path.join(image_path,"".join(["resized_",image_name])))
-	
-	elif not (res_w is None) and not (res_h is None) :
-	
-		#Make the new image half the width and half the height of the original image
-		resized_im = im.resize((round(im.size[0]*res_w), round(im.size[1]*res_h)))
+		if not (res_p is None):
+			
+			#Make the new image half the width and half the height of the original image
+			resized_im = im.resize((round(im.size[0]*res_p), round(im.size[1]*res_p)))
 
-		#Save the cropped image
-		resized_im.save(os.path.join(image_path,"".join(["resized_",image_name])))
-
-	elif not (res_w is None) and (res_h is None) :
-	
-		#Make the new image half the width and half the height of the original image
-		resized_im = im.resize((round(im.size[0]*res_w), round(im.size[1]*1)))
-
-		#Save the cropped image
-		resized_im.save(os.path.join(image_path,"".join(["resized_",image_name])))
-
-	elif (res_w is None) and not (res_h is None) :
-	
-		#Make the new image half the width and half the height of the original image
-		resized_im = im.resize((round(im.size[0]*1), round(im.size[1]*res_h)))
-
-		#Save the cropped image
-		resized_im.save(os.path.join(image_path,"".join(["resized_",image_name])))
-	else :
-		print("Please Enter valid arguments !!")
+			#Save the cropped image
+			resized_im.save(os.path.join(dir_path,"".join(["resized_",image_name])))
 		
+		elif not (res_w is None) and not (res_h is None) :
+		
+			#Make the new image half the width and half the height of the original image
+			resized_im = im.resize((round(im.size[0]*res_w), round(im.size[1]*res_h)))
+
+			#Save the cropped image
+			resized_im.save(os.path.join(dir_path,"".join(["resized_",image_name])))
+
+		elif not (res_w is None) and (res_h is None) :
+		
+			#Make the new image half the width and half the height of the original image
+			resized_im = im.resize((round(im.size[0]*res_w), round(im.size[1]*1)))
+
+			#Save the cropped image
+			resized_im.save(os.path.join(dir_path,"".join(["resized_",image_name])))
+
+		elif (res_w is None) and not (res_h is None) :
+		
+			#Make the new image half the width and half the height of the original image
+			resized_im = im.resize((round(im.size[0]*1), round(im.size[1]*res_h)))
+
+			#Save the cropped image
+			resized_im.save(os.path.join(dir_path,"".join(["resized_",image_name])))
+		else :
+			print("Please Enter valid arguments !!")
+			
 parser = argparse.ArgumentParser(description = """
 											Select one of the following :
 											res_p : Resize by user determined percentage (proportional)
 											res_w : resize by user determined width (proportional) 
 											res_h : resize by user determined height (proportional) 	
 												""")
-parser.add_argument('-im',
-						'--image_name',
+parser.add_argument('-dir',
+						'--dir_path',
 						type = str,
-						help = 'Input jpeg image name')
-
-parser.add_argument('-p',
-						'--image_path',
-						type = str,
-						help = 'Input jpeg image folder path')
+						help = 'Input images folder path')
 
 parser.add_argument('-res_p',
 						'--image_resize_proportion',
@@ -85,5 +85,4 @@ parser.add_argument('-res_h',
 args = parser.parse_args()
 
 if __name__ == '__main__':
-	
-	image_resize(args.image_name,args.image_path,args.image_resize_proportion,args.image_width_resize_proportion,args.image_height_resize_proportion)
+	image_resize(args.dir_path,args.image_resize_proportion,args.image_width_resize_proportion,args.image_height_resize_proportion)
