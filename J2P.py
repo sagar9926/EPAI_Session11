5c23902c7cbcf930cd219fa3f097ce4cefbc86f7
sagar9926 <sagarkumar9926@gmail.com>
2020-10-20 20:48:13 +0530

    Update J2P.py

--- J2P.py
+++ J2P.py
@@ -17,17 +17,17 @@ except OSError as error:
 
 
 def jpeg_to_png(image_name,image_path):
-	if not os.path.isdir(image_path):
-		raise ValueError('Input path is not a Directory')
-	if(image_name.find(".jpg") == -1):
-		raise ValueError("Input image is not jpg")
-
-	image_rel_path = os.path.join(image_path,image_name)
+    print(image_path)
+    print(os.path.isdir(image_path))
+    if not os.path.isdir(image_path):
+        raise ValueError('Input path is not a Directory')
+    if(image_name.find(".jpg") == -1):
+        raise ValueError("Input image is not jpg")
+    image_rel_path = os.path.join(image_path,image_name)
 	#image_abs_path = os.path.abspath(image_rel_path)
-	
-	im = Image.open(image_rel_path)
-	rgb_im = im.convert('RGB')
-	rgb_im.save(os.path.join(path,"".join([image_name[:-4],'_jpg_to_png','.png'])))
+    im = Image.open(image_rel_path)
+    rgb_im = im.convert('RGB')
+    rgb_im.save(os.path.join(path,"".join([image_name[:-4],'_jpg_to_png','.png'])))
 
 
 if __name__ == '__main__':
