a5b2587f035c7db515d97580f736725a76688c48
sagar9926 <sagarkumar9926@gmail.com>
2020-10-20 14:52:26 +0530

    Update P2J.py

--- P2J.py
+++ P2J.py
@@ -5,14 +5,29 @@ import os
 
 print(f'loading P2J.py: __name__ = {__name__}')
 
+print("Creating a new directory to store Converted Png images :")
+directory = "Converted_to_jpg"
+path = os.path.join('.',directory)
+
+try: 
+	os.makedirs(path, exist_ok = True) 
+	print("Directory '%s' created successfully" % directory) 
+except OSError as error: 
+	print("Directory '%s' can not be created" % directory) 
+
+
 def png_to_jpeg(image_name,image_path):
+	if not os.path.isdir(image_path):
+		raise ValueError('Input path is not a Directory')
+	if(image_name.find(".png") == -1):
+		raise ValueError("Input image is not png")
 	
 	image_rel_path = os.path.join(image_path,image_name)
 	#image_abs_path = os.path.abspath(image_rel_path)
 	
 	im = Image.open(image_rel_path)
 	rgb_im = im.convert('RGB')
-	rgb_im.save(os.path.join(image_path,"".join([image_name[:-4],'_png_to_jpg','.jpeg'])))
+	rgb_im.save(os.path.join(path,"".join([image_name[:-4],'_png_to_jpg','.jpeg'])))
 
 
 if __name__ == '__main__':
